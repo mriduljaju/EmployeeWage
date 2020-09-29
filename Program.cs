f06e4aa72a40926da4adc85acbe87a9fea39a726
mriduljaju <mriduljaju123@gmail.com>
2020-09-29 07:52:38 +0530

    [Mridul] Refactor :Calculate Daily Employee Wage

--- Program.cs
+++ Program.cs
@@ -8,16 +8,26 @@ namespace EmployeeWage
         {
             //Constants
             int IsFullTime = 1;
+            int EmpRatePerHour = 20;
+            //Variables
+            int empWage = 0;
+            int empHr = 0;
+
             Random random = new Random();
+            //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IsFullTime)
             {
                 Console.WriteLine("Employee is Present.");
+                empHr = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent.");
+                Console.WriteLine("Employee is Absent!");
+                empHr = 0;
             }
+            empWage = empHr * EmpRatePerHour;
+            Console.WriteLine("Employee Wage = " + empWage);
         }
     }
 }
