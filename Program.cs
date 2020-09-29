b6764f963397128b0da4db5503318d00721c7033
mriduljaju <mriduljaju123@gmail.com>
2020-09-29 08:08:33 +0530

    [Mridul] Refactor: Calculating Wages for a month

--- Program.cs
+++ Program.cs
@@ -6,34 +6,41 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            public const int IsPartTime = 1;
-        public const int IsFullTime = 2;
-        public const int EmpRatePerHr = 20;
 
-        static void Main(string[] args)
-        {
-            //Variables
-            int empWage = 0;
-            int empHr = 0;
-            Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IsPartTime:
-                    empHr = 4;
-                    break;
+		  public const int IsPartTime = 1;
+		public const int IsFullTime = 2;
+		public const int EmpRatePerHr = 20;
+		public const int NumOfWorkingDays = 2;
+		static void Main(string[] args)
+		{
+			//Variables
+			int empWage = 0;
+			int empHr = 0;
+			int TotalEmpWage = 0;
 
-                case IsFullTime:
-                    empHr = 8;
-                    break;
+			//Computation
+			for (int Day = 0; Day < NumOfWorkingDays; Day++)
+			{
+				Random random = new Random();
+				int empCheck = random.next(0, 3);
+				switch (empCheck)
+				{
+					case IsPartTime:
+						empHr = 4;
+						break;
+					case IsFullTime:
+						empHr = 8;
+						break;
+					default:
+						empHr = 0;
+						break;
+				}
 
-                default:
-                    empHr = 0;
-                    break;
-            }
-            empWage = empHr * EmpRatePerHr;
-            Console.WriteLine("Employee Wage = " + empWage);
-        }
+				empWage = empHr * EmpRatePerHr;
+				TotalEmpWage += empWage;
+				Console.WriteLine("Employee Wage = " + empWage);
+			}
+			Console.WriteLine("Total Emp Wage = " + TotalEmpWage);
+		}
     }
 }
