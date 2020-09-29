1ac4a364595e24b3d7f420eb763accd414d4db3d
mriduljaju <mriduljaju123@gmail.com>
2020-09-29 07:45:42 +0530

    [Mridul] Refactor:Check Employee Presence

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Problem");
+            //Constants
+            int IsFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
         }
     }
 }
